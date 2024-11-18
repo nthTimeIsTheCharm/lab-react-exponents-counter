cbe7bc80fe63c0ffa790d348238d134c670108c4
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2024-11-18 21:31:11 +0100

    Almost refeactored. Struggling with strings and repeat().

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
 import "./App.css";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
+/* import ExponentTwo from "./components/ExponentTwo";
 import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import ExponentSix from "./components/ExponentSix"; */
+import Exponent from "./components/Exponent";
+import { useState } from "react";
 
 
 function App () {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} increment={increment} decrement={decrement} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        {/* <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} /> */}
+        <Exponent count={count} exponent={2}/>
+        <Exponent count={count} exponent={3}/>
+        <Exponent count={count} exponent={4}/>
+        <Exponent count={count} exponent={5}/>
+        <Exponent count={count} exponent={6}/>
       </div>
     </div>
   );
